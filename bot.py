f79f77d661efe4f11022eb32abb81c88a2bb7d96
Aditya Madan <aditya.madan7@gmail.com>
2019-09-24 18:10:46 -0400

    Completed Stock aggregating and display functions

--- bot.py
+++ bot.py
@@ -1,24 +1,34 @@
 import discord
+from discord import Game
 from alpha_vantage.timeseries import TimeSeries
+from discord.ext import commands
+import matplotlib.pyplot as plt
 
-client = discord.Client()
-ts = TimeSeries(key='XXXXX', output_format='pandas')
+ts = TimeSeries(key='X', output_format='pandas') #Hid Token
+bot = commands.Bot(command_prefix='$')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    game = discord.Game("with the market")
-    await client.change_presence(status=discord.Status, activity=game)
+    print('We have logged in as {}'.format(bot.user.name))
+    # await bot.change_presence(game=Game(name="With the market"))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(pass_context=True)
+async def stock(ctx, a: str):
+    data, meta_data = ts.get_intraday(symbol=a, interval='1min', outputsize='full')
+    data['4. close'].plot()
+    plt.title('Intraday Times Series for the {} stock (1 min)'.format(a))
+    plt.savefig('graph.png')
+    file = discord.File('graph.png', filename='graph.png')
+    embed = discord.Embed()
+    embed.set_image(url="attachment://graph.png")
+    await ctx.send(file=file, embed=embed)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
-client.run("XXXX.XXUxMA.85a0cV6h91Kye1wpfOSARdgIfIk")
+@bot.command(pass_context=True)
+async def hello(ctx):
+    await ctx.send('Hello there!')
 
+
+bot.run("X") #Hid Token
